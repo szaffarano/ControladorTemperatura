345e29672b5378c64c261f89dc342e3debd67f95
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2013-01-03 21:19:18 -0300

    avance en el menu con el control remoto

---
+++ src/YK001.h
@@ -0,0 +1,28 @@
+#include "Key.h"
+
+#ifndef YK001_H
+#define YK001_H
+
+static unsigned long POWER = 0xFFA25D;
+static unsigned long MODE = 0xFF629D;
+static unsigned long MUTE = 0xFFE21D;
+static unsigned long PREV = 0xFF22DD;
+static unsigned long NEXT = 0xFF02FD;
+static unsigned long PAUSE = 0xFFC23D;
+static unsigned long VOL_MIN = 0xFFE01F;
+static unsigned long VOL_PLUS = 0xFFA857;
+static unsigned long EQ = 0xFF906F;
+static unsigned long DIGIT_0 = 0xFF6897;
+static unsigned long HUNDERT_PLUS = 0xFF9867;
+static unsigned long BACK = 0xFFB04F;
+static unsigned long DIGIT_1 = 0xFF30CF;
+static unsigned long DIGIT_2 = 0xFF18E7;
+static unsigned long DIGIT_3 = 0xFF7A85;
+static unsigned long DIGIT_4 = 0xFF10EF;
+static unsigned long DIGIT_5 = 0xFF38C7;
+static unsigned long DIGIT_6 = 0xFF5AA5;
+static unsigned long DIGIT_7 = 0xFF42BD;
+static unsigned long DIGIT_8 = 0xFF4AB5;
+static unsigned long DIGIT_9 = 0xFF52AD;
+
+#endif
--- src/sketch.ino
+++ src/sketch.ino
@@ -3,13 +3,24 @@
 #include <LiquidCrystal_SR.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <IRremote.h>
+#include "YK001.h"
 
 Clock* clock;
 LiquidCrystal_SR* lcd;
 DallasTemperature* sensors;
 OneWire* one_wire;
+IRrecv* receiver;
+decode_results* results;
 
 Date date;
+
+void (*screens[])() = {
+  print_temp,
+  print_date
+};
+uint8_t screens_count;
+uint8_t current_screen;
 
 #define RED 7
 #define GREEN 6
@@ -17,13 +28,13 @@ Date date;
 
 #define HC595_CLOCK 1
 #define HC595_DATA 3
-#define HC595_ENABLED 4
+#define HC595_LATCH 4
 
 #define RTC_DS 2
 
 #define DS18B20 8
 
-#define IR_RECEIVER 11
+#define RECEIVER 11
 
 void setup() {
   // inicializar el RTC
@@ -35,48 +46,49 @@ void setup() {
   // leds de status
   setup_leds();
   
+  // sensor de temperatura
   setup_sensors();
+  
+  // control remoto
+  setup_receiver();
+
+  screens_count = sizeof(screens)/sizeof(void*);
+  current_screen = 0;
 }
  
 void loop() {
-  clock->getDate(&date);
-
-  lcd->clear();
-  
-  lcd->setCursor(0, 0);  
-  String str = "";
-  str += pad(date.getDay()) + "/";
-  str += pad(date.getMonth()) + "/";
-  str += String(date.getYear()) + " ";
-  lcd->print(str);
-  
-  lcd->setCursor(0, 1);
-  str = "";
-  str += pad(date.getHour()) + ":";
-  str += pad(date.getMinute()) + ":";
-  str += pad(date.getSecond());
-  lcd->print(str);
-
-  delay(1500);
-  
-  sensors->requestTemperatures();
-  lcd->clear();
-  lcd->print(sensors->getTempCByIndex(0));
-  lcd->print(" grados");
-  
-  delay(1500);
+  if (receiver->decode(results)) {
+    if (results->value == NEXT) {
+      current_screen = current_screen < screens_count-1 ? current_screen+1 : 0;
+    } else if (results->value == PREV) {
+      current_screen = current_screen > 0 ? current_screen-1 : screens_count-1;
+    }
+    receiver->resume();
+  }
+  screens[current_screen]();
+  delay(1000);
 }
 
 static void setup_lcd() {
-  lcd = new LiquidCrystal_SR (HC595_DATA, HC595_CLOCK, HC595_ENABLED);
+  lcd = new LiquidCrystal_SR (HC595_DATA, HC595_CLOCK, HC595_LATCH);
   lcd->begin (16,2);
+  lcd->noBacklight();
   lcd->home ();
 }
 
 static void setup_leds() {
+  
   pinMode(WHITE, OUTPUT);
   pinMode(GREEN, OUTPUT);
   pinMode(RED, OUTPUT);
+  
+  pinMode(HC595_LATCH, OUTPUT);
+  pinMode(HC595_CLOCK, OUTPUT);
+  pinMode(HC595_DATA, OUTPUT);
+  
+  digitalWrite(HC595_LATCH, LOW);  
+  shiftOut(HC595_DATA, HC595_CLOCK, LSBFIRST, 0);
+  digitalWrite(HC595_LATCH, HIGH);  
   
   digitalWrite(WHITE, HIGH);
   delay(500);
@@ -97,6 +109,38 @@ static void setup_sensors() {
   sensors->begin();
 }
 
+static void setup_receiver() {
+  receiver = new IRrecv(RECEIVER);
+  receiver->enableIRIn();
+}
+
 static String pad(uint8_t number) {
   return (number < 10 ? "0" : "") + String(number);
+}
+
+static void print_date() {
+  clock->getDate(&date);
+
+  lcd->clear();
+  
+  lcd->setCursor(0, 0);  
+  String str = "";
+  str += pad(date.getDay()) + "/";
+  str += pad(date.getMonth()) + "/";
+  str += String(date.getYear()) + " ";
+  lcd->print(str);
+  
+  lcd->setCursor(0, 1);
+  str = "";
+  str += pad(date.getHour()) + ":";
+  str += pad(date.getMinute()) + ":";
+  str += pad(date.getSecond());
+  lcd->print(str);
+}
+
+static void print_temp() {
+  sensors->requestTemperatures();
+  lcd->clear();
+  lcd->print(sensors->getTempCByIndex(0));
+  lcd->print(" grados");
 }
